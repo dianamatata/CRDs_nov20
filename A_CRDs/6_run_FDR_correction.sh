d499a213621fc6f659076f308e45602f841eb4f7
dianamatata <diana.avalos.m@gmail.com>
2020-11-23 10:18:37 +0100

    filter FDR

--- A_CRDs/6_run_FDR_correction.sh
+++ A_CRDs/6_run_FDR_correction.sh
@@ -1,2 +1,9 @@
-#cat gene_CRD_mean_chunk*.txt | gzip -c > gene_CRD_mean_permutations_full.txt.gz
-Rscript 6bis_runFDR_cis.R gene_CRD_mean_permutations_full.txt.gz 0.05 gene_CRD_mean_permutations
+#!/bin/bash
+INDIR=/home/users/a/avalosma/scratch/5_CRDgene/merged
+OUTDIR=/home/users/a/avalosma/scratch/5_CRDgene/significants
+mkdir -p $OUTDIR
+
+for file in $INDIR/*.txt.gz; do
+	filename=$(echo $file | rev | cut -d "/" -f1 | rev)
+	Rscript 6bis_runFDR_cis.R $INDIR/$filename 0.05 $OUTDIR/$filename
+done
