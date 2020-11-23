1ac2570a67a62b1783c5e7ac2a565e2d39a25a86
dianamatata <diana.avalos.m@gmail.com>
2020-11-23 10:18:18 +0100

    run % on cluster

--- A_CRDs/5b_run_5.sh
+++ A_CRDs/5b_run_5.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cmd="source 5_run_mapping_CRD_gene.sh"
-wsbatch -J 5_script.job --partition=mono-EL7 --time=10:10:00 -o 5_script.out -e 5_script.err --wrap="$cmd"
+cmd="source /home/users/a/avalosma/IMMUNE_CELLS/diana_scripts/A_CRDs/5_run_mapping_CRD_gene.sh"
+wsbatch -J 5script.job --partition=mono-EL7 --time=04:00:00 -o 5script.out -e 5script.err --wrap="$cmd"
 
