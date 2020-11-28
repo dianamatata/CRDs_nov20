9ed7662f87b3c3a22c18fbe4ccbb44aeeb853378
dianamatata <diana.avalos.m@gmail.com>
2020-11-28 16:01:15 +0100

    merge trans diff chr

---
+++ A_CRDs/trans/21.1_merge_chr.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+OUTDIR=/home/users/a/avalosma/scratch/7_CRD_Trans
+
+### concatenate at the end
+mkdir -p $OUTDIR/merged
+
+for data_type in  'methyl' 'hist' ; do
+        for cell_type in 'neut' 'mono' 'tcell' ; do
+                for module in 'mean' 'loom' ; do
+			OUT=$OUTDIR/first/${data_type}_${cell_type}_${module}_chr_${c0}_${c1}_trans
+			cat $OUTDIR/first/${data_type}_${cell_type}_${module}_* \
+			> $OUTDIR/merged/${data_type}_${cell_type}_${module}_trans_ALL.txt.gz
+                done
+        done
+done
