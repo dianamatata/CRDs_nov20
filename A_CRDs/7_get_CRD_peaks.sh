b2111818258fff20d0f8f74848125a753b2f428e
dianamatata <diana.avalos.m@gmail.com>
2020-12-08 19:28:23 +0100

    info and peaks draft

---
+++ A_CRDs/7_get_CRD_peaks.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+CLOMICs=/home/users/a/avalosma/bin/clomics/bin/clomics
+DATADIR=/home/users/a/avalosma/scratch/2_CRD
+DATA_TREE=/home/users/a/avalosma/scratch/1_CRD
+OUT_FOLDER=/home/users/a/avalosma/scratch/8_CRD_PEAKS
+mkdir -p $OUT_FOLDER $OUT_FOLDER/chr $OUT_FOLDER/ALL
+
+for data_type in  'methyl' 'hist' ; do
+        for cell_type in 'neut' 'mono' 'tcell' ; do
+                for module in 'mean' 'loom' ; do
+			LM=$DATADIR/quantify_ALL/${data_type}_${cell_type}.ALLchr.${module}.txt.gz
+			name=${data_type}_${cell_type}_${module}
+			for c in $(seq 1 22); do
+				LT=$DATA_TREE/${data_type}_${cell_type}.chr$c\.tree.txt.gz
+			        LO=$OUT_FOLDER/chr/${name}.chr$c\.peak
+        		        cmd="$CLOMICs gpeak --tree $LT --mod $LM --out ${LO}.txt.gz"
+                		eval $cmd
+				# JOB=chr${c}_peak_$name
+				# wsbatch -J $JOB\.job --partition=mono-shared-EL7 --time=00:00:15 --wrap="$cmd"
+			done
+		done
+	done
+done
+
+
+# merge
+for data_type in  'methyl' 'hist' ; do
+        for cell_type in 'neut' 'mono' 'tcell' ; do
+                for module in 'mean' 'loom' ; do
+                        LM=$DATADIR/quantify_ALL/${data_type}_${cell_type}.ALLchr.${module}.txt.gz
+                        for c in $(seq 1 22); do
+	                	LO=$OUT_FOLDER/chr/${name}.chr$c\.peak
+        	        	LO_ALL=$OUT_FOLDER/ALL/${name}.ALLchr.peaks.txt
+                		zcat $LO\.txt.gz >> $LO_ALL
+			done
+                done
+        done
+done
+
+
