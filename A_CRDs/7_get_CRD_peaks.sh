a86eaeaf9f046238bfcef17758a07e251021e397
dianamatata <diana.avalos.m@gmail.com>
2020-12-13 10:52:52 +0100

    modified LM according to 7.1

--- A_CRDs/7_get_CRD_peaks.sh
+++ A_CRDs/7_get_CRD_peaks.sh
@@ -6,10 +6,12 @@ DATA_TREE=/home/users/a/avalosma/scratch/1_CRD
 OUT_FOLDER=/home/users/a/avalosma/scratch/8_CRD_PEAKS
 mkdir -p $OUT_FOLDER $OUT_FOLDER/chr $OUT_FOLDER/ALL
 
+# LM is only the list of CRD names, so look at 7.1 first
+
 for data_type in  'methyl' 'hist' ; do
         for cell_type in 'neut' 'mono' 'tcell' ; do
                 for module in 'mean' 'loom' ; do
-			LM=$DATADIR/quantify_ALL/${data_type}_${cell_type}.ALLchr.${module}.txt.gz
+			LM=$DATADIR/CRD_names/${data_type}_${cell_type}.ALLchr.${module}.txt
 			name=${data_type}_${cell_type}_${module}
 			for c in $(seq 1 22); do
 				LT=$DATA_TREE/${data_type}_${cell_type}.chr$c\.tree.txt.gz
