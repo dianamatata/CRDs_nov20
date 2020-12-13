7751f1cd96b2ab78fe53c9d76998e913cbe66618
dianamatata <diana.avalos.m@gmail.com>
2020-12-13 11:55:01 +0100

    formatting

--- A_CRDs/trans/21.1_merge_chr.sh
+++ A_CRDs/trans/21.1_merge_chr.sh
@@ -7,8 +7,8 @@ mkdir -p $OUTDIR/merged
 for data_type in  'methyl' 'hist' ; do
         for cell_type in 'neut' 'mono' 'tcell' ; do
                 for module in 'mean' 'loom' ; do
-			OUT=$OUTDIR/first/${data_type}_${cell_type}_${module}_chr_${c0}_${c1}_trans
-			cat $OUTDIR/first/${data_type}_${cell_type}_${module}_* \
+			OUT=$OUTDIR/OUTsimple/${data_type}_${cell_type}_${module}_chr_${c0}_${c1}_trans
+			cat $OUTDIR/OUTsimple/${data_type}_${cell_type}_${module}_* \
 			> $OUTDIR/merged/${data_type}_${cell_type}_${module}_trans_ALL.txt.gz
                 done
         done
