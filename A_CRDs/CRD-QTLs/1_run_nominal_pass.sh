221d92edfea0b8f14791875ec41ece4f3a37f063
dianaavalos <diana.avalos.m@gmail.com>
2021-01-17 10:26:43 +0100

    norminal and permut CRD-QTLs

--- A_CRDs/CRD-QTLs/1_run_nominal_pass.sh
+++ A_CRDs/CRD-QTLs/1_run_nominal_pass.sh
@@ -1,34 +1,32 @@
 #!/bin/bash
-permuts=200
-K=100
 
 VCF=/home/users/a/avalosma/scratch/10_CRD_QTLs/All_chr.BPWP10_13_12_15.vcf.gz
 DATACRD=/home/users/a/avalosma/scratch/2_CRD
 OUTDIR=/home/users/a/avalosma/scratch/10_CRD_QTLs
-mkdir -p $OUTDIR $OUTDIR/permuts_$permuts
-mkdir -p $OUTDIR/merged_$permuts
+mkdir -p $OUTDIR $OUTDIR/nominal $OUTDIR/merged_nominal
+K=100
+FDR=0.05
 
 for data_type in  'methyl' 'hist' ; do
   for cell_type in 'neut' 'mono' 'tcell' ; do
     for module in 'mean' 'loom' ; do
       BED=$DATACRD/quantify_ALL/${data_type}_${cell_type}.ALLchr.${module}.txt.gz
-			for k in $(seq 1 $K); do
-
-        OUT1=$OUTDIR/permuts_$permuts/${data_type}_${cell_type}_${module}_CRD_QTL_$k
-				cmd="QTLtools cis --vcf $VCF --bed $BED --permute $permuts  --chunk $k $K --out ${OUT1}.txt"
-				eval $cmd
-
-			done
-		done
-	done
+      for k in $(seq 1 $K); do
+        OUT1=$OUTDIR/nominal/${data_type}_${cell_type}_${module}_CRD_QTL_$k
+        cmd="QTLtools cis --vcf $VCF --bed $BED --nominal $FDR  --chunk $k $K --out ${OUT1}.txt"
+        eval $cmd
+      done
+    done
+  done
 done
 
 ### concatenate at the end
+
 for data_type in  'methyl' 'hist' ; do
   for cell_type in 'neut' 'mono' 'tcell' ; do
     for module in 'mean' 'loom' ; do
       name=${data_type}_${cell_type}_${module}
-      cat $OUTDIR/permuts_$permuts/${name}_*.txt | gzip -c > $OUTDIR/merged_$permuts/${name}_${condition}_ALL.txt.gz
+      cat $OUTDIR/nominal/${name}_*.txt | gzip -c > $OUTDIR/merged_nominal/${name}_${condition}_ALL.txt.gz
     done
   done
 done
